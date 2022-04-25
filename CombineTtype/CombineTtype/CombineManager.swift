3a5e6943b6789bf30cf23c33980c8a290b0528c0
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-04-25 23:20:18 +0300

    Fix combine manager

---
+++ CombineTtype/CombineTtype/CombineManager.swift
@@ -0,0 +1,71 @@
+//
+//  CombineManager.swift
+//  CombineTtype
+//
+//  Created by mehmet karanlık on 25.04.2022.
+//
+
+import Foundation
+import Combine
+
+
+
+struct CombineManager {
+
+ let defaultTimeout : TimeInterval = TimeInterval(15)
+
+ func send<T:Codable>( modelToDecode model : T.Type,    cancellableSet :inout Set<AnyCancellable>,url :URL, body : [String:Any]? , requestType : RequestType, completion : @escaping (T?) -> Void) throws  {
+
+  var urlRequest = URLRequest(url: url,timeoutInterval: defaultTimeout)
+  urlRequest.httpMethod = requestType.rawValue
+
+  var data : T?
+
+  // handle body if there is
+  if let body = body {
+   do {
+    let decodedData = try JSONSerialization.data(withJSONObject: body, options: .prettyPrinted)
+    urlRequest.httpBody = decodedData
+   } catch let e {
+    throw e
+   }
+  }
+
+  URLSession.shared.dataTaskPublisher(for: urlRequest)
+   .tryMap(handleOutput)
+   .decode(type: T.self, decoder: JSONDecoder())
+   .sink { sinkCompletion  in
+    switch sinkCompletion{
+     case .finished:
+      break
+     case .failure(_):
+      break
+    }
+   } receiveValue: { receivedData in
+    data = receivedData
+    if let decodedData = data { completion(decodedData)  }
+    // define cancellabel either on where you call or either via DI
+   }
+   .store(in: &cancellableSet)
+
+
+
+
+
+
+  func handleOutput(output: URLSession.DataTaskPublisher.Output) throws  -> Data {
+   guard
+    let response = output.response as? HTTPURLResponse,
+    response.statusCode >= 200 && response.statusCode < 300 else {
+    throw URLError(.serverCertificateUntrusted)
+   }
+   return output.data
+  }
+ }
+}
+
+enum RequestType : String {
+ case GET,POST,PUT,DELETE
+}
+
+
--- CombineTtype/CombineTtype/ContentView.swift
+++ CombineTtype/CombineTtype/ContentView.swift
@@ -43,15 +43,21 @@ struct ContentView_Previews: PreviewProvider {
 class CombineTTypeViewModel : ObservableObject {
 
  let service = CombineTTypeService()
+ let url : URL = URL(string: "https://jsonplaceholder.typicode.com/posts")!
 
  @Published var posts : [Post?] = []
 
  var cancellabels = Set<AnyCancellable>()
 
  init() {
-  service.getPosts(modelToDecode: [Post].self, completion: { [weak self] data in
+
+ }
+
+ func fetchPosts() {
+  service.getPosts(modelToDecode: [Post].self, cancellableSet: &cancellabels, url: url) { [weak self] data in
    self?.posts = data ?? []
-  }, cancellableSet: &cancellabels, url: URL(string: "https://jsonplaceholder.typicode.com/posts")!)
+  }
+
  }
 
 
@@ -60,37 +66,20 @@ class CombineTTypeViewModel : ObservableObject {
 
 struct CombineTTypeService : CombineTTypeServiceProtocol {
 
- func getPosts< R : Codable >(
-  modelToDecode model : R.Type,
-  completion : @escaping (R?) -> Void,
-  cancellableSet :inout Set<AnyCancellable>,
-  url :URL
- ) {
 
-  var data : R?
-
-  let url = url
-
-  URLSession.shared.dataTaskPublisher(for: url)
-   .receive(on: DispatchQueue.main)
-   .tryMap(handleOutput)
-   .decode(type: R.self , decoder: JSONDecoder())
-   .sink { (completion) in
-    print("DEBUG: Completion is \(completion)")
-   } receiveValue: { returnedPosts in
-    data = returnedPosts
-    if let decodedData = data { completion(decodedData) }
-   }.store(in: &cancellableSet)
- }
+ var manager: CombineManager = CombineManager()
 
 
- func handleOutput(output: URLSession.DataTaskPublisher.Output) throws  -> Data {
-  guard
-   let response = output.response as? HTTPURLResponse,
-   response.statusCode >= 200 && response.statusCode < 300 else {
-   throw URLError(.serverCertificateUntrusted)
+ func getPosts< R : Codable >(modelToDecode model : R.Type,cancellableSet :inout Set<AnyCancellable>,url :URL,completion : @escaping (R?) -> Void) {
+
+  do {
+   try manager.send(modelToDecode: model,
+                    cancellableSet: &cancellableSet,
+                    url: url, body: nil,
+                    requestType: .GET) { response in  completion(response) }
+  } catch let e {
+   print(e)
   }
-  return output.data
  }
 }
 
@@ -99,10 +88,12 @@ protocol CombineTTypeServiceProtocol {
 
  func getPosts<R: Codable>(
   modelToDecode model : R.Type,
-  completion :  @escaping (R?) -> Void,
   cancellableSet :inout Set<AnyCancellable>,
-  url :URL
+  url :URL,
+  completion : @escaping (R?) -> Void
  )
+
+ var manager : CombineManager { get }
 
 }
 
